14770f7d9a42e97dd79466e1aeda922ecad03491
Erin Parker <erinchanparker@gmail.com>
2013-12-05 14:02:37 -0800

    first commit

---
+++ Superheroine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Superheroine
+//
+//  Created by Erin Parker on 12/5/13.
+//  Copyright (c) 2013 Erin Parker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Superheroine/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Superheroine
+//
+//  Created by Erin Parker on 12/5/13.
+//  Copyright (c) 2013 Erin Parker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

997a154fcad4a61b04d22d7ac7e1fb37a13a83f3
Erin Parker <erinchanparker@gmail.com>
2013-12-06 02:45:00 -0800

    UIScrollView as part of CreateCard now working

--- Superheroine/CreateCardViewController.h
+++ Superheroine/CreateCardViewController.h
@@ -8,6 +8,5 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CreateCardViewController : UIViewController
-
+@interface CreateCardViewController : UIViewController <UIScrollViewDelegate>
 @end
--- Superheroine/CreateCardViewController.m
+++ Superheroine/CreateCardViewController.m
@@ -11,6 +11,11 @@
 
 @interface CreateCardViewController ()
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *menuButton;
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) IBOutlet UIView *contentView;
+@property (strong, nonatomic) IBOutlet UILabel *label1;
+@property (strong, nonatomic) IBOutlet UILabel *label2;
+
 @end
 
 @implementation CreateCardViewController
@@ -22,6 +27,25 @@
     [self.menuButton setTarget:self.revealViewController];
     [self.menuButton setAction:@selector(revealToggle:)];
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    self.contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 910)];
+    
+
+    self.label1.autoresizingMask = UIViewAutoresizingNone;
+
+    self.label2.autoresizingMask = UIViewAutoresizingNone;
+    
+    [self.contentView addSubview:self.label1];
+    [self.contentView addSubview:self.label2];
+    
+    self.scrollView.delegate = self;
+    [self.scrollView setScrollEnabled:YES];
+
+    [self.scrollView setContentSize:CGSizeMake(320, 910)];
+    [self.scrollView addSubview:self.contentView];
+    [self.view addSubview:self.scrollView];
+    
 }
 
 
